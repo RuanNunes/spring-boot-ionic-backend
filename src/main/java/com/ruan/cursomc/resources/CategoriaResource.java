393e0aff0d9c36a3cdf26e830c93f99fc7e75127
RuanNunes <ruanschwanz@gmail.com>
2019-07-19 15:39:09 -0300

    Criando operação de intanciação

--- src/main/java/com/ruan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ruan/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,12 @@
 package com.ruan.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ruan.cursomc.domain.Categoria;
 import com.ruan.cursomc.services.CategoriaService;
 
 @RestController
@@ -19,8 +15,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value= "{/id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> find(@RequestParam("id") Integer id) {
 		return ResponseEntity.ok().body(service.buscar(id));
 	}
 	
