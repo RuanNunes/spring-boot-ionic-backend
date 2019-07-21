cc1779a593b3dae0d834e87cbf818269b8dfd31b
RuanNunes <ruanschwanz@gmail.com>
2019-07-21 11:07:38 -0300

    Adicionado domains de estado e cidade e também implementado relacionamentos de insert

--- src/main/java/com/ruan/cursomc/CursomcApplication.java
+++ src/main/java/com/ruan/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.ruan.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ruan.cursomc.domain.Categoria;
+import com.ruan.cursomc.domain.Cidade;
+import com.ruan.cursomc.domain.Estado;
 import com.ruan.cursomc.domain.Produto;
 import com.ruan.cursomc.repositories.CategoriaRepository;
+import com.ruan.cursomc.repositories.CidadeRepository;
+import com.ruan.cursomc.repositories.EstadoRepository;
 import com.ruan.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,6 +23,14 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -38,8 +51,26 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		//associando estados as cidades
+		Cidade c1 = new Cidade(null, "Uberlandia");
+		c1.setEstado(est1);
+		Cidade c2 = new Cidade(null, "São Paulo");
+		c2.setEstado(est2);
+		Cidade c3 = new Cidade(null, "Campinas");
+		c3.setEstado(est2);
+		
+		//associando cidades aos estados
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		//salvando os objetos
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}	
 
 }
---
+++ src/main/java/com/ruan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.ruan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ruan.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
