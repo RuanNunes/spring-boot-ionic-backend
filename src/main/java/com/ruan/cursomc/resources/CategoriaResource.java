4e91151a061c6d8d67fc1e490313006974b2de78
RuanNunes <ruanschwanz@gmail.com>
2019-07-22 17:19:39 -0300

    inserindo uma nova Categoria com post

--- src/main/java/com/ruan/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ruan/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,18 @@
 package com.ruan.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ruan.cursomc.domains.Categoria;
 import com.ruan.cursomc.services.CategoriaService;
 
 @RestController
@@ -15,9 +21,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<?> find(@RequestParam("id") Integer id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(service.buscar(id));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		//retorna url com novo registro inserido
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/ruan/cursomc/resources/ClienteResource.java
+++ src/main/java/com/ruan/cursomc/resources/ClienteResource.java
@@ -2,6 +2,7 @@ package com.ruan.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,8 +16,8 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<?> find(@RequestParam("id") Integer id) {
+	@RequestMapping(value = "/{id}" , method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		return ResponseEntity.ok().body(service.buscar(id));
 	}
 	
