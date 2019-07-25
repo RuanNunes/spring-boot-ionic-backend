0b7eab8ec95933a1cb14f61cd22979b533ecef2c
Ruan Nunes <ruanschwanz@gmail.com>
2019-07-25 10:38:19 -0300

    Criando profile desenvolvimento mysql e profile desenvolvimento postgres

---
+++ src/main/java/com/ruan/cursomc/config/DevPostgresConfig.java
@@ -0,0 +1,28 @@
+package com.ruan.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ruan.cursomc.services.DbService;
+
+@Configuration
+@Profile("devpostgres")
+public class DevPostgresConfig {
+	@Autowired
+	private DbService dbService;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	@Bean
+	public boolean instantiateDateBase() throws ParseException {
+		if(!"create".equals(strategy))
+			return false;
+		
+		dbService.instatiateTestDataBase();
+		return true;
+	}
+}
